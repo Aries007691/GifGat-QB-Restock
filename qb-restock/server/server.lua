9955b823a4e962bfec2fb562fd3977be5837df4e
Accidental Genius 2.0 <110291471+Aries007691@users.noreply.github.com>
2023-03-11 11:51:42 +0200

    Update server.lua

--- qb-restock/server/server.lua
+++ qb-restock/server/server.lua
@@ -6,14 +6,14 @@ local function round(x)
     return x >= 0 and math.floor(x + 0.5) or math.ceil(x - 0.5)
 end
 
-RegisterServerEvent('cardealer:server:removemony', function(data, vehicle)
+RegisterServerEvent('qb-restock:server:removemony', function(data, vehicle)
     vehicle = data.vehicle
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
     local steamname = GetPlayerName(src)
 local discount = round(data.price * Config.discount)
     if Player.PlayerData.money.cash >= discount  then
-        TriggerClientEvent("car:client:create_blip", src, data.vehicle)  
+        TriggerClientEvent("qb-restock:client:create_blip", src, data.vehicle)  
         Player.Functions.RemoveMoney("cash", discount)
         TriggerClientEvent('QBCore:Notify', src, 'Vehicle Successfully Bought', "success")   
     else
@@ -23,7 +23,7 @@ end)
 
 
 
-RegisterServerEvent('cardealer:owncar', function(k, vehicle)
+RegisterServerEvent('qb-restock:owncar', function(k, vehicle)
         local stock = nil
         local car = vehicle
         local isthere = false
@@ -68,28 +68,4 @@ RegisterNetEvent('qb-stock:server:updatestock', function(data)
 end) 
 
 
-
-local function pdsendLogs (message)
-    if message == nil or message == '' then return FALSE end
-    PerformHttpRequest('https://discord.com/api/webhooks/1083827004920561714/i7STkfP8LH9OUQNB3oicvByuUTgJLX1frMG4OlShG2FSp4XwwiZOR_j-81v_SjGlPkkp', function(err, text, headers) end, 'POST', json.encode({ content = message }), { ['Content-Type'] = 'application/json' })
-end
-
-RegisterServerEvent('qb-stock')
-AddEventHandler('qb-stock', function(message)
-pdsendLogs(message)
-end)
-
-
-RegisterNetEvent('qb-stock:server:discord', function(name, amount)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local playername = Player.PlayerData.name
-    local playerlicense =Player.PlayerData.license    
-    local cid = Player.PlayerData.citizenid  
-    local job = Player.PlayerData.job.name
-    local rank = Player.PlayerData.job.grade.level  
-    local firstname =  Player.PlayerData.charinfo.firstname 
-    local lastname =  Player.PlayerData.charinfo.lastname
-        TriggerEvent('qb-stock', '```diff\n- Bought a car\n```\n```css\n[Sender]: "Nissan"\n[Reciever]: '..playername..'\n[Charname]: '..firstname..'\n[CharLastname]: '..lastname..'\n[Cid]: '..cid..'\n[License]: '..playerlicense..'\n[Job]: '..job..'\n[rank]: '..rank..'```\n')       
-end)
 
